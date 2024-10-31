ab0f6b1a1e8a00f4b4fa50e767a49d738e2668c9
dev-rajnish <dev.rajnish@proton.me>
2024-11-01 01:05:14 +0530

    Auto Commit and Push

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
 USR_BIN_DIR="$HOME/.local/bin"
-BIN_FILE="$USR_BIN_DIR/f2g"
 
-#sudo zypper --no-gpg-checks --no-refresh in --allow-unsigned-rpm --no-recommends -y -l  -n  gh
+echo 'started'
 
-curl -o $BIN_FILE https://raw.githubusercontent.com/dev-rajnish/bash-exec/master/folder-to-github/folder-to-github ||
-  echo Download failed!
-chmod +x $BIN_FILE
+cp -rv git/ $USR_BIN_DIR
 
-echo -e ":::: >> Installed f2g at $USR_BIN_DIR \n"
-
-echo -e ":::: >> Run \" f2g \" to push modified change \n"
-
-echo -e ":::: >> Run \" f2g . \" for add files and push all \n"
-
-echo -e ":::: >> Install gh if not installed \n"
+echo -e ":::: >> Installed"
